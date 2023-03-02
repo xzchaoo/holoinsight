91fe1c27c4775c6a66d39485bd63c16f3f45afc1
xiangfeng.xzc <xiangfeng.xzc@antfin.com>
2023-03-02 12:04:03 +0800

    xftest

--- server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/ApiKeyService.java
+++ server/home/home-service/src/main/java/io/holoinsight/server/home/biz/service/ApiKeyService.java
@@ -12,6 +12,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @version 1.0: ApiKeyService.java, v 0.1 2022年05月31日 11:27 上午 jinsong.yjs Exp $
  */
 public interface ApiKeyService extends IService<ApiKey> {
-  ApiKey queryById(Long id, String tenant);
+  ApiKey queryById(Long id, String tenant22);
 
 }

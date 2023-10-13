d70c87d40860606565d74fa1902dd2057a449c0f
xzchaoo <xzchaoo@gmail.com>
2023-10-13 17:11:02 +0800

    fix: fix db keyword

--- server/extension/extension-common-flyway/src/main/resources/db/migration/V22__231011_CREATE_agg_TABLE.sql
+++ server/extension/extension-common-flyway/src/main/resources/db/migration/V22__231011_CREATE_agg_TABLE.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS `agg_offset_v1` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `gmt_create` datetime NOT NULL,
   `gmt_modified` datetime NOT NULL,
-  `partition` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `partition_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `consumer_group` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `version` bigint NOT NULL,
   `data` blob,
   PRIMARY KEY (`id`),
-  KEY `k_partition_version` (`partition`,`consumer_group`)
+  KEY `k_partition_name_consumer_group` (`partition_name`,`consumer_group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `agg_task_v1` (
@@ -26,9 +26,9 @@ CREATE TABLE IF NOT EXISTS `agg_state_v1` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `gmt_create` datetime NOT NULL,
   `gmt_modified` datetime NOT NULL,
-  `partition` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `partition_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `consumer_group` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `state` longblob,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_partition` (`partition`, `consumer_group`)
+  UNIQUE KEY `uk_partition_name_consumer_group` (`partition_name`, `consumer_group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
